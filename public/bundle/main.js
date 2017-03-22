243eb73f95be5b24c7bec96c90c0511a58dc0bac
Zorqie <zorqie@yahoo.com>
2017-03-22 00:31:29 -0700

    Fixed localhost->gyps

--- public/bundle/main.js
+++ public/bundle/main.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "f6c9b14e44a33c8c44ea"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "f79f8f73174d56ae611e"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
@@ -9266,8 +9266,8 @@ var io = __webpack_require__(464);
 
 // FIXME this should be in configuration somewhere.
 // Establish a Socket.io connection
-var socket = io('http://localhost:2017');
-// const socket = io('https://gyps.herokuapp.com/'); 
+// const socket = io('http://localhost:2017');
+var socket = io('https://gyps.herokuapp.com/');
 // Initialize our Feathers client application through Socket.io
 // with hooks and authentication.
 var app = feathers().configure(socketio(socket)).configure(hooks())
