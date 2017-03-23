854a3363e5b5155d0975576d3bd69a661396adbf
Zorqie <zorqie@yahoo.com>
2017-03-22 19:17:05 -0700

    Still fixing Fb auth

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const reactPath = path.resolve(__dirname, 'node_modules\\react-toolbox');
+
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -9,13 +9,13 @@ module.exports = {
 	entry: {
 		// './src/index.js',
 		// app:  './public/app.jsx',
-		main: './public/gyps/main.jsx',
+		main: './public/main.jsx',
 		// vapp: './public/vapp.jsx',
 		// appt: './public/appt.jsx'
 	},
 	output: {
 		filename: '[name].js',
-		path: './public/gyps/bundle'
+		path: path.resolve(__dirname,'public\\bundle')
 	},
 	module: {
 		rules: [
@@ -28,7 +28,7 @@ module.exports = {
 
 			{
 				test: /\.s?css$/,
-				loader: ExtractTextPlugin.extract({
+				use: ExtractTextPlugin.extract({
 					fallback: {loader: "style-loader"},
 					loader: 'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
 				})
@@ -36,6 +36,21 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: false,
+                keep_fnames: false
+            },
+            compress: {
+                screw_ie8: false
+            },
+            comments: false
+        }),
 		new ExtractTextPlugin({filename: '[name].css', allChunks: true }),
 		// new webpack.optimize.OccurenceOrderPlugin(),
 		// new webpack.optimize.CommonsChunkPlugin({
